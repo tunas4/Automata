24b289bdebb21fb07b52df5e0aa063c8215d4670
noahx001 <lopeznoe191119@gmail.com>
2024-09-27 20:15:11 -0600

    automatas 1.2.1

--- Automata/automata.py
+++ Automata/automata.py
@@ -88,7 +88,7 @@ def validar(automata):
         seguir = False
 
     return seguir
-ejemplo = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9","@","#","&","%","$"]
+
 if(validar(automata)):
     print("Cadena Valida")
 else:
