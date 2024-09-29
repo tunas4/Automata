253f7a246c89fd422193b8dec18312282f6072de
noahx001 <lopeznoe191119@gmail.com>
2024-09-28 18:35:14 -0600

    automata 2.5.4

--- Automata/clases/ventana_mostrar_creditos.py
+++ Automata/clases/ventana_mostrar_creditos.py
@@ -5,9 +5,15 @@ class MostrarCreditos:
         creditos_ventana = tk.Toplevel(root)
         creditos_ventana.geometry("300x150")
         creditos_ventana.title("Créditos")
+
+        label = tk.Label(creditos_ventana, text="Grado: 4 | Sección: A")
+        label.pack()
+
+        label0 = tk.Label(creditos_ventana, text="Asignatura: Estructura de Datos y Aplicadas")
+        label0.pack()
         
-        label1 = tk.Label(creditos_ventana, text="Jonathan Ivan Castro Saenz")
+        label1 = tk.Label(creditos_ventana, text="Jonathan Ivan Castro Saenz | 23170035")
         label1.pack(pady=30)
 
-        label2 = tk.Label(creditos_ventana, text="Noe Abel Vargas López")
+        label2 = tk.Label(creditos_ventana, text="Noe Abel Vargas López | 23170106")
         label2.pack()
